385c51a3e28eb53d27b22fa1b1c0b38345eef555
Soxbear <soxbear2@gmail.com>
2022-10-27 18:16:59 -0700

    WIP Player Code

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, Hittable
 {
+    [Header("Stats")]
     public int Health;
+
+    [Header("Settings")]
+    public int MaxHealth;
+    public float Speed;
+
+    Rigidbody Body;
+
+    PlayerMain Controls;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Controls = new PlayerMain();
+        Controls.Enable();
+        Controls.Main.Movement.performed += (e) =>
+        {
+            MoveVector = e.ReadValue<Vector2>();
+        };
+
+        Body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,8 +34,11 @@ public class Player : MonoBehaviour, Hittable
         
     }
 
+    Vector2 MoveVector;
+
     void FixedUpdate() {
-        
+        Vector2 TargetMoveVector = (Vector2)transform.position + (MoveVector.normalized * Speed * Time.fixedDeltaTime);
+        Body.MovePosition(new Vector3(TargetMoveVector.x, 0f, TargetMoveVector.y));
     }
 
     public void Hit(int Damage, Vector2 Direction) {
